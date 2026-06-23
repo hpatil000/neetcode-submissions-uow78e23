d729975b84146e1c93b4235e8c0ffe61d837b881
Harshita Patil <117500937+hpatil000@users.noreply.github.com>
2026-06-23 18:45:25 -0400

    Add: sql-highest-grade-for-each-student - submission-1

---
+++ SQL/sql-highest-grade-for-each-student/submission-1.sql
@@ -0,0 +1,65 @@
+-- Write your query below
+-- 
+
+-- select student_id, exam_id, score
+--     from (select student_id, exam_id, score,
+--             Row_number() over(partition by student_id order by score DESC , exam_id ASC) as ranking 
+--             from exam_results
+--         ) t
+--     where ranking = 1;
+
+with t as (
+    select student_id, exam_id, score,
+    row_number () over (partition by student_id order by score DESC, exam_id ASC) as ranking
+ from exam_results
+)
+
+select student_id, exam_id, score from t 
+where ranking = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select distinct on (student_id)
+-- --     student_id,
+-- --     exam_id, 
+-- --     score
+-- -- from exam_results
+-- -- order by student_id, score DESC, exam_id; 
+
+
+
+
+
+
+
+
+
