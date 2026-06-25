4218cfe4259248b1071a452b48b3ff55d8dd3678
Harshita Patil <117500937+hpatil000@users.noreply.github.com>
2026-06-24 21:26:42 -0400

    Add: sql-team-scores-in-football-tournament - submission-0

---
+++ SQL/sql-team-scores-in-football-tournament/submission-0.sql
@@ -0,0 +1,39 @@
+-- Write your query below
+
+select t.team_id, t.team_name,      
+    SUM 
+    (CASE
+        WHEN t.team_id = m.host_team AND m.host_goals > m.guest_goals THEN 3
+        WHEN t.team_id = m.guest_team AND m.host_goals < m.guest_goals THEN 3
+        WHEN m.host_goals = m.guest_goals THEN 1
+
+        ELSE 0
+    END ) as num_points
+
+FROM teams t
+LEFT JOIN matches m
+on t.team_id = m.host_team OR t.team_id = m.guest_team
+GROUP BY t.team_id, t.team_name
+ORDER BY num_points DESC, t.team_id ASC;
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
